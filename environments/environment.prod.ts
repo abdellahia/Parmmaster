de89da9255f1da3419a2d5240c10f57637ef107d
abdellahia <46005419+abdellahia@users.noreply.github.com>
2019-08-01 13:01:11 +0200

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  requiredRoles: ['r.app-parmmaster.user'],
+  apiUrl: 'https://slayer.kob05084.koehlerpaper.com/api',
+  keycloakUrl: 'https://keycloak.kob05084.koehlerpaper.com/auth',
+  keycloakClient: 'app-parmmaster'
+};
